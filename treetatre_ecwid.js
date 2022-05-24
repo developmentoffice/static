9aa8f2980b8bfc4d31591506cc0138ef0866ee45
developmentoffice <aleksey_mstu@bk.ru>
2022-05-24 14:54:46 +0300

    Init VK counter

--- treetatre_ecwid.js
+++ treetatre_ecwid.js
@@ -1,3 +1,18 @@
-window.addEventListener('load', () => {
-  console.log('...');
-});
+const Controller = {
+    init() {
+        this.initCounter();
+    },
+    initCounter() {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = 'https://vk.com/js/api/openapi.js?169';
+        script.onload = function () {
+            VK.Retargeting.Init('VK-RTRG-1406368-1bFSm');
+            VK.Retargeting.Hit();
+        };
+        document.head.appendChild(script);
+    }
+};
+
+window.addEventListener('load', () => Controller.init());
